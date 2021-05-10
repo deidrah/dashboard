9325e7827e582fd6d18227839b41f682d7789f85
Deidrah <dominikastasik93@gmail.com>
2021-05-10 21:39:14 +0200

    add general statistics page

---
+++ js/script.js
@@ -0,0 +1,12 @@
+/*
+
+function toggleMenu(visible) {
+  document.querySelector('.menu').classList.toggle('show', visible)
+}
+
+document.querySelector('.hamburger').addEventListener('click', function(e) {
+  e.preventDefault();
+  toggleMenu()
+});
+
+*/
